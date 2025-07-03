f54c7838ff1d5cf2d5e327eea415225dfb9354c5
Aneesh Phatak <aneesh.m.phatak@gmail.com>
2025-07-04 01:24:02 +0530

    trying to deploy backend

--- api/models.py
+++ api/models.py
@@ -1,8 +1,10 @@
 from pymongo import MongoClient
+import os
 
 #client = MongoClient("mongodb://localhost:27017/")
-client = MongoClient("mongodb+srv://aneeshmphatak:malayamarutham@cluster0.lzwebsp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-db = client['typingtestdb']
+MONGO_URI = os.getenv("MONGO_URI")
+client = MongoClient(MONGO_URI)
 
+db = client['typingtestdb']
 users_collection = db['users']
 scores_collection = db['scores']
